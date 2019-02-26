9f5aef00593cf4c19a044f168a1244eaafdf93f3
Jimmy Zebroski and Tanner Damron <jimmy@>
2019-02-26 13:21:01 -0800

    add linking to database and start date input added

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,70 +1,146 @@
-using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
-    public class Item
+  public class Item
+  {
+    private string _description;
+    private string _date;
+    private int _id;
+
+    public Item (string description, int id = 0, string date)
     {
-
-        private string _description;
-        private int _id;
-
-        public Item(string description)
-        {
-            _description = description;
-            _id = _instances.Count;
-        }
-
-        public string GetDescription()
-        {
-            return _description;
-        }
-
-        public void SetDescription(string newDescription)
-        {
-            _description = newDescription;
-        }
-
-        public static List<Item> GetAll()
-        {
-            List<Item> allItems = new List<Item> { };
-            MySqlConnection conn = DB.Connection();
-            conn.Open();
-            MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-            cmd.CommandText = @"SELECT * FROM items;";
-            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-
-            while (rdr.Read())
-            {
-                int itemId = rdr.GetInt32(0);
-                string itemDescription = rdr.GetString(1);
-                Item newItem = new Item(itemDescription, itemId);
-                allItems.Add(newItem);
-            }
-
-            conn.Close();
-
-            if (conn != null)
-            {
-                conn.Dispose();
-            }
-
-            return allItems;
-        }
-
-        // public static void ClearAll()
-        // {
-        //     _instances.Clear();
-        // }
-
-        // public int GetId()
-        // {
-        //     return _id;
-        // }
-
-        // public static Item Find(int searchId)
-        // {
-        //     return _instances[searchId - 1];
-        // }
+      _description = description;
+      _id = id;
+      _date = date;
     }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public string GetDescription()
+    {
+      return _description;
+    }
+
+    public void SetDescription(string newDescription)
+    {
+      _description = newDescription;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static List<Item> GetAll()
+    {
+      Item dummyItem = new Item("dummy item");
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+     if (conn != null)
+     {
+      conn.Dispose();
+     }
+    }
+
+   public static Item Find(int id)
+   {
+     MySqlConnection conn = DB.Connection();
+     conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = id;
+      cmd.Parameters.Add(thisId); 
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemDescription = "";
+      while (rdr.Read())
+      {
+       itemId = rdr.GetInt32(0);
+       itemDescription = rdr.GetString(1);
+      }
+          Item foundItem= new Item(itemDescription, itemId); 
+
+
+     // more logic will go here!
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundItem;
+   }
+
+   public override bool Equals(System.Object otherItem)
+  {
+    if (!(otherItem is Item))
+    {
+      return false;
+    }
+    else
+    {
+      Item newItem = (Item) otherItem;
+      bool idEquality = (this.GetId() == newItem.GetId());
+      bool descriptionEquality = (this.GetDescription() == newItem.GetDescription());
+      return (idEquality && descriptionEquality);
+    }
+  }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter description = new MySqlParameter();
+      description.ParameterName = "@ItemDescription";
+      description.Value = this._description;
+      cmd.Parameters.Add(description);    
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+
+      // more logic will go here
+
+        conn.Close();
+        if (conn != null)
+        {
+          conn.Dispose();
+        }
+    }
+
+  }
 }
